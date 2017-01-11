15ef6a329a1f4bcba2a3bb28e0dc01200fcefc77
JOSEMANUEL\JoseManuel <wino46@hotmail.com>
2017-01-11 11:50:39 +0100

    ProyectoMVC4_DB_Stored_Procedures
    Proyecto MVC4 con Procedimientos Almacenados de Base de Datos SQL Server

---
+++ ProyectoMVC4_DB_Stored_Procedures/ProyectoMVC4_DB_Stored_Procedures/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoMVC4_DB_Stored_Procedures
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ ProyectoMVC4_DB_Stored_Procedures/ProyectoMVC4_DB_Stored_Procedures/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProyectoMVC4_DB_Stored_Procedures.MvcApplication" Language="C#" %>
